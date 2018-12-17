6aaebc532b1d53d753442dace7d4b12d641017b2
DewiHu <44896143+DewiHu@users.noreply.github.com>
2018-12-17 14:28:43 +0100

    Added SP's
    added dbo.sp_pubquiz_insert
    added dbo.sp_evenement_update
    added dbo.sp_evenement_delete

--- SP_BANG_Dewi_Hu.sql
+++ SP_BANG_Dewi_Hu.sql
@@ -1,6 +1,8 @@
 USE BANG
 GO
 
+DROP PROC dbo.sp_evenement_update
+DROP PROC dbo.sp_evenement_delete
 DROP PROC dbo.sp_evenement_add
 DROP PROC dbo.sp_evenement_insert
 DROP PROC dbo.sp_locatie_insert
@@ -9,6 +11,7 @@ DROP PROC dbo.sp_evenement_select
 DROP PROC dbo.sp_top100_insert
 DROP PROC dbo.sp_top100_delete
 DROP PROC dbo.sp_top100_update
+DROP PROC dbo.sp_pubquiz_insert
 GO
 
 /*
@@ -382,8 +385,8 @@ GO
 DELETE EVENT
 */
 
-/*
 CREATE PROCEDURE dbo.sp_evenement_delete
+@E_ID int
 AS
 BEGIN  
 	DECLARE @savepoint varchar(128) = CAST(OBJECT_NAME(@@PROCID) as varchar(125)) + CAST(@@NESTLEVEL AS varchar(3))
@@ -392,7 +395,11 @@ BEGIN
 		BEGIN TRANSACTION
 		SAVE TRANSACTION @savepoint
 
-		--als flow tot dit punt komt transactie counter met 1 verlagen
+		IF EXISTS (SELECT * FROM EVENEMENT WHERE E_ID = @E_ID)
+			DELETE FROM EVENEMENT WHERE E_ID = @E_ID
+		ELSE
+			THROW 50204, 'Het evenement bestaat niet', 1
+
 		COMMIT TRANSACTION 
 	END TRY	  
 	BEGIN CATCH
@@ -414,14 +421,69 @@ BEGIN
 	END CATCH
 END;	
 GO
-*/
 
 /*
 UPDATE EVENT
 */
 
-/*
 CREATE PROCEDURE dbo.sp_evenement_update
+@E_NAAM varchar(256),
+@E_DATUM date,
+@E_ID int,
+@LOCATIENAAM varchar(256),
+@PLAATSNAAM varchar(1024),
+@ADRES varchar(1024),
+@HUISNUMMER int
+AS
+BEGIN  
+	DECLARE @savepoint varchar(128) = CAST(OBJECT_NAME(@@PROCID) as varchar(125)) + CAST(@@NESTLEVEL AS varchar(3))
+	DECLARE @startTrancount int = @@TRANCOUNT;
+	BEGIN TRY
+		BEGIN TRANSACTION
+		SAVE TRANSACTION @savepoint
+
+		IF EXISTS (SELECT * FROM EVENEMENT WHERE E_ID = @E_ID)
+		BEGIN
+			IF EXISTS (SELECT * FROM LOCATIE WHERE PLAATSNAAM = @PLAATSNAAM AND ADRES = @ADRES AND HUISNUMMER = @HUISNUMMER)
+			UPDATE EVENEMENT
+			SET E_NAAM = @E_NAAM, E_DATUM = @E_DATUM
+			ELSE
+			EXEC dbo.sp_locatie_insert @LOCATIENAAM = @LOCATIENAAM, @PLAATSNAAM = @PLAATSNAAM, @ADRES = @ADRES, @HUISNUMMER = @HUISNUMMER
+			UPDATE EVENEMENT
+			SET E_NAAM = @E_NAAM, E_DATUM = @E_DATUM, PLAATSNAAM = @PLAATSNAAM, ADRES = @ADRES, HUISNUMMER = @HUISNUMMER
+		END
+		ELSE
+			THROW 50204, 'Het evenement bestaat niet', 1
+
+		COMMIT TRANSACTION 
+	END TRY	  
+	BEGIN CATCH
+		IF XACT_STATE() = -1 and @startTrancount = 0  -- "doomed" transaction, eigen context only
+			BEGIN
+				ROLLBACK TRANSACTION
+				PRINT 'Buitentran state -1 eigen context'
+			END
+		ELSE IF XACT_STATE() = 1 --transactie dus niet doomed, maar wel in error state 						 --je zit immers niet voor nop in het CATCH blok
+			BEGIN
+				ROLLBACK TRANSACTION @savepoint --werk van deze sproc ongedaan gemaakt
+				COMMIT TRANSACTION --trancount 1 omlaag
+				PRINT 'Buitentran state 1 met trancount ' + cast(@startTrancount as varchar)
+			END
+			DECLARE @errormessage varchar(2000) 
+			SET @errormessage ='Een fout is opgetreden in procedure ''' + object_name(@@procid) + '''.
+			Originele boodschap: ''' + ERROR_MESSAGE() + '''';
+			THROW 50200, @errormessage, 1
+	END CATCH
+END
+GO
+
+/*
+INSERT PUBQUIZ
+*/
+
+CREATE PROCEDURE dbo.sp_pubquiz_insert 
+@E_ID INT,
+@TITLE VARCHAR(256)
 AS
 BEGIN  
 	DECLARE @savepoint varchar(128) = CAST(OBJECT_NAME(@@PROCID) as varchar(125)) + CAST(@@NESTLEVEL AS varchar(3))
@@ -431,6 +493,12 @@ BEGIN
 		SAVE TRANSACTION @savepoint
 		
 		--checks hier
+		IF EXISTS (SELECT * FROM PUBQUIZ WHERE E_ID = @E_ID)
+			THROW 50205, 'Er is al een pubquiz bij dit evenement', 1
+		ELSE
+			--succes operatie hier
+			INSERT INTO PUBQUIZ (E_ID, TITLE)
+			VALUES (@E_ID, @TITLE)
 
 		--als flow tot dit punt komt transactie counter met 1 verlagen
 		COMMIT TRANSACTION 
@@ -454,8 +522,3 @@ BEGIN
 	END CATCH
 END;	
 GO
-*/
-
-BEGIN TRAN
-EXEC dbo.sp_evenement_select_all
-ROLLBACK TRAN
