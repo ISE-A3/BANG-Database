4df058ee1f0a3827ca9a43fd62e4dfc4693f9c09
elsh5544 <j.elshof@outlook.com>
2018-12-20 14:24:07 +0100

    fixed issue where login for BANG_FRONT_END could not connect to db after execution of db setup batch file because the user and their rights were not setup

--- BANG_database_setup.bat
+++ BANG_database_setup.bat
@@ -23,3 +23,8 @@ for %%f in (.\stored_procedures\*.sql) do (
 	ECHO Creating stored procedure %%~f>> %OUTPUT%
 	%SQLCMD% -S %SERVER% -d %DB% -U %LOGIN% -P %PASSWORD% -i %%~f >> %OUTPUT%
 )
+
+ECHO Setting up user rights>> %OUTPUT%
+ECHO Setting up user rights
+
+%SQLCMD% -S %SERVER% -d %DB% -U %LOGIN% -P %PASSWORD% -i database_setup\BANG_userSetup.sql  >>%OUTPUT%
