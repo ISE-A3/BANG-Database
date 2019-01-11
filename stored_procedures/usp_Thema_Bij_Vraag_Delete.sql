900be6a24107fd795632e78e43e886c126a4546d
IamDC <diegocup@live.nl>
2019-01-11 14:45:40 +0100

    geen nuttige commit, niks veranderd

--- stored_procedures/usp_Thema_Bij_Vraag_Delete.sql
+++ stored_procedures/usp_Thema_Bij_Vraag_Delete.sql
@@ -2,8 +2,8 @@ USE BANG
 GO
 
 CREATE OR ALTER PROCEDURE dbo.usp_Thema_Bij_Vraag_Delete
-@VRAAG_NAAM varchar(256) NOT NULL,
-@THEMA varchar(256) NOT NULL
+@VRAAG_NAAM varchar(256),
+@THEMA varchar(256) = NULL
 AS
 BEGIN  
 	DECLARE @savepoint varchar(128) = CAST(OBJECT_NAME(@@PROCID) as varchar(125)) + CAST(@@NESTLEVEL AS varchar(3))
@@ -12,19 +12,26 @@ BEGIN
 		BEGIN TRANSACTION
 		SAVE TRANSACTION @savepoint
 
+		IF NOT EXISTS (SELECT '' FROM VRAAG WHERE VRAAG_NAAM = @VRAAG_NAAM)
+			THROW 50229, 'De vraag bestaat niet.', 1
+
 		DECLARE @VRAAG_ID int = (SELECT VRAAG_ID FROM VRAAG WHERE VRAAG_NAAM = @VRAAG_NAAM)
 
-		IF EXISTS (SELECT '' FROM THEMA_BIJ_VRAAG WHERE VRAAG_ID = @VRAAG_ID)
-			IF EXISTS (SELECT '' FROM THEMA_BIJ_VRAAG WHERE VRAAG_ID = @VRAAG_ID AND THEMA = @THEMA)
-				DELETE FROM THEMA_BIJ_VRAAG
-				WHERE VRAAG_ID = @VRAAG_ID AND THEMA = @THEMA 
-
-				IF NOT EXISTS (SELECT '' FROM THEMA_BIJ_VRAAG WHERE THEMA = @THEMA)
-					EXEC dbo.usp_Thema_Delete @THEMA
-			ELSE
-				THROW 50226, 'Het thema bij de vraag kan niet verwijderd worden, want de vraag heeft dit thema niet.', 1
-		ELSE 
+		IF NOT EXISTS (SELECT '' FROM THEMA_BIJ_VRAAG WHERE VRAAG_ID = @VRAAG_ID)
 			THROW 50224, 'De vraag heeft geen thema(''s).', 1
+
+		IF @THEMA IS NOT NULL
+			IF NOT EXISTS (SELECT '' FROM THEMA_BIJ_VRAAG WHERE VRAAG_ID = @VRAAG_ID AND THEMA = @THEMA)
+				THROW 50226, 'Het thema bij de vraag kan niet verwijderd worden, want de vraag heeft dit thema niet.', 1
+
+		IF @THEMA IS NULL
+			DELETE FROM THEMA_BIJ_VRAAG
+			WHERE VRAAG_ID = @VRAAG_ID
+
+		DELETE FROM THEMA_BIJ_VRAAG
+		WHERE VRAAG_ID = @VRAAG_ID AND THEMA = @THEMA 
+
+		EXEC dbo.usp_Thema_Delete
 
 		--als flow tot dit punt komt transactie counter met 1 verlagen
 		COMMIT TRANSACTION 
