86706fa3199908b769fd5cff0ee6aeca0e3582e1
Dewi <d.hu@student.han.nl>
2019-01-15 14:43:25 +0100

    Updated Unit tests related to Pubquiz

--- unit_testing/Unit_Test_Pubquiz_Insert.sql
+++ unit_testing/Unit_Test_Pubquiz_Insert.sql
@@ -8,18 +8,30 @@ BEGIN
 	EXEC tSQLt.FakeTable 'dbo', 'PUBQUIZ';
 	EXEC tSQLt.FakeTable 'dbo', 'EVENEMENT';
 
-	EXEC [tSQLt].[ExpectNoException] 
-
 	IF OBJECTPROPERTY(OBJECT_ID('[dbo].[EVENEMENT]'), 'TableHasIdentity') = 1
 	SET IDENTITY_INSERT [dbo].[EVENEMENT] ON
 
 	INSERT INTO dbo.EVENEMENT(EVENEMENT_NAAM, EVENEMENT_DATUM, EVENEMENT_ID, PLAATSNAAM, ADRES, HUISNUMMER, HUISNUMMER_TOEVOEGING)
 	VALUES('Pubquiz1', '1999-02-02', 1, 'Cuijk', 'Beerseweg', 15, 'B')
 	
+	--TEST 1) Test voor het invoeren van pubquiz
+	EXEC [tSQLt].[ExpectNoException] 
 	EXECUTE dbo.usp_Pubquiz_Insert 'Pubquiz1', 'Pubquiz_Insert_test';
-	--EXECUTE dbo.usp_Pubquiz_Insert 'Pubquiz1', 'Pubquiz_Insert_test2';	--test voor multiple executes en check op dubbele waardes 
-	--EXECUTE usp_Pubquiz_Insert 'Pubquiz2', 'Pubquiz_Insert_test2';	--test om te controleren op evenementen die niet bestaan
 
+	/*
+	--TEST 2) Test voor multiple executes en check op dubbele waardes 
+	EXEC [tSQLt].[ExpectException] @ExpectedMessage = 'Een fout is opgetreden in procedure ''usp_Pubquiz_Insert''.
+			Originele boodschap: ''Er is al een pubquiz bij dit evenement'''
+	EXECUTE dbo.usp_Pubquiz_Insert 'Pubquiz1', 'Pubquiz_Insert_test';
+	EXECUTE dbo.usp_Pubquiz_Insert 'Pubquiz1', 'Pubquiz_Insert_test2';	
+	*/
+
+	/*
+	--TEST 3) test om te controleren op evenementen die niet bestaan
+	EXEC [tSQLt].[ExpectException] @ExpectedMessage = 'Een fout is opgetreden in procedure ''usp_Pubquiz_Insert''.
+			Originele boodschap: ''Dit evenement bestaat niet meer'''
+	EXECUTE dbo.usp_Pubquiz_Insert 'Pubquiz2', 'Pubquiz_Insert_test2';		
+	*/
 END
 GO
 
