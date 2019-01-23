7ee6e36eabaeee9d2e023505882590282e5f6d70
Martijn Landman <martijn.landman@hotmail.nl>
2019-01-23 11:48:04 +0100

    Commit voor checkout

--- stored_procedures/usp_Team_Insert.sql
+++ stored_procedures/usp_Team_Insert.sql
@@ -2,8 +2,8 @@ USE BANG;
 GO
 
 CREATE or ALTER PROCEDURE dbo.usp_Team_Insert
-	@parameter numeric(2),
-	@parameters numeric(7,2)
+	@EVENEMENT_NAAM VARCHAR(256),
+	@TEAM_NAAM VARCHAR(256)
 AS
 BEGIN  
 	DECLARE @savepoint varchar(128) = CAST(OBJECT_NAME(@@PROCID) as varchar(125)) + CAST(@@NESTLEVEL AS varchar(3))
@@ -13,6 +13,28 @@ BEGIN
 		SAVE TRANSACTION @savepoint
 		
 		--checks hier
+		IF NOT EXISTS(
+			SELECT ''
+			FROM PUBQUIZ
+			WHERE EVENEMENT_ID = (
+				SELECT EVENEMENT_ID
+				FROM EVENEMENT
+				WHERE EVENEMENT_NAAM = @EVENEMENT_NAAM
+				)
+			)
+			THROW 50010, 'Deze pubquiz bestaat niet', 1
+
+		IF EXISTS (
+			SELECT ''
+			FROM TEAM
+			WHERE TEAM_NAAM = @TEAM_NAAM
+			AND EVENEMENT_ID = (
+				SELECT EVENEMENT_ID
+				FROM EVENEMENT
+				WHERE EVENEMENT_NAAM = @EVENEMENT_NAAM
+				)
+			)
+			THROW 50011, 'Bij deze pubquiz is er al een team met deze teamnaam', 1
 
 		--succes operatie hier
 
